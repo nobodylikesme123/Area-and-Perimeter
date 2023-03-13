d7f41a4f4bcded52e95de8af68d8f4875edb124a
Zavier Philpot <>
2023-03-13 01:56:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+  Name: 
+  James Hargest College
+  Programming Internal for 2.7 & 2.8 ~ 12 credits
+  Due: 6 April 2023
+  
+  TIP: Use assessment guide to help guide you through this Internal
+'''
