e7b68ef88df763bdde44cb72ed33b12d78a7b2bd
Zavier Philpot <127706249+nobodylikesme123@users.noreply.github.com>
2023-03-23 21:38:08 +0000

    allowing the user to select the shape

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 '''
-  Name: 
-  James Hargest College
-  Programming Internal for 2.7 & 2.8 ~ 12 credits
-  Due: 6 April 2023
-  
-  TIP: Use assessment guide to help guide you through this Internal
+Zavier Philpot
+Area and Perimeter
+
 '''
+
+import tkinter as tk
+
+def on_shape_selected(shape):
+    print(f"You selected the {shape} shape")
+
+root = tk.Tk()
+root.title("Select a shape")
+
+circle_btn = tk.Button(root, text="Circle", command=lambda: on_shape_selected("circle"))
+circle_btn.pack()
+
+triangle_btn = tk.Button(root, text="Triangle", command=lambda: on_shape_selected("triangle"))
+triangle_btn.pack()
+
+square_btn = tk.Button(root, text="Square", command=lambda: on_shape_selected("square"))
+square_btn.pack()
+
+rectangle_btn = tk.Button(root, text="Rectangle", command=lambda: on_shape_selected("rectangle"))
+rectangle_btn.pack()
+
+root.mainloop()
